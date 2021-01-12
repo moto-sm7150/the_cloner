636a3b940293db024bc6e9f0f16db39c61c7b313
kytøaq ™ <65746867+kytoaq@users.noreply.github.com>
2021-01-12 01:32:56 -0600

    the_cloner: Rebrand Organization
    It may sound silly, but they are different chipsets.
    It might seem that they are similar, but in the promoted information
    by Qualcomm, "sdmmagpie" is part of the sm7150 family,
    therefore I have moved this repository here to avoid mixing
    chipsets based on their function. 
    The Snapdragon 675 is part of the "sm6150" family (the original indeed).
    
    Signed-off-by: kytoaq <kytoaq@gmail.com>

--- odessa_staging.sh
+++ odessa_staging.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2020 kytoaq <kytoaq@gmail.com>
-# Copyright (C) 2020 - 2021 Motorola™ SM6150
+# Copyright (C) 2020 - 2021 Motorola™ SM7150
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,35 +22,35 @@ GITHUB="https://github.com"
 GITLAB="https:/gitlab.com"
 
 # Organization List
-SM6150_COMMON="moto-sm6150"
+SM7150_COMMON="moto-sm7150"
 NEMESIS_DEVELOPERS="NemesisDevelopers"
 DORITOS_MOTOROLA="DoritosMotorola"
 
 # Repositories List
 REPO_DEVICE="device_motorola_odessa"
-REPO_DEVICE_COMMON="device_motorola_sm6150-common"
+REPO_DEVICE_COMMON="device_motorola_sm7150-common"
 REPO_VENDOR="vendor_motorola"
-REPO_VENDOR_COMMON="vendor_motorola_sm6150-common"
-REPO_KERNEL="kernel_motorola_sm6150"
-REPO_SEPOLICY="sepolicy_sm6150-common"
+REPO_VENDOR_COMMON="vendor_motorola_sm7150-common"
+REPO_KERNEL="kernel_motorola_sm7150"
+REPO_SEPOLICY="sepolicy_sm7150-common"
 
 # Optional Repositories
-SEPOLICY="sepolicy_sm6150-common"
+SEPOLICY="sepolicy_sm7150-common"
 
 # Set Branches for Repositories
 STAGING="staging/lineage-17.1"
 
 # Motorola Paths
 PATH_DEVICE="device/motorola/odessa"
-PATH_DEVIE_COMMON="device/motorola/sm6150-common"
+PATH_DEVIE_COMMON="device/motorola/sm7150-common"
 PATH_VENDOR="vendor/motorola"
-PATH_VENDOR_COMMON="vendor/motorola/sm6150-common"
-PATH_SEPOLICY="device/motorola/sm6150-common/sepolicy"
-PATH_KERNEL="kernel/motorola/sm6150"
+PATH_VENDOR_COMMON="vendor/motorola/sm7150-common"
+PATH_SEPOLICY="device/motorola/sm7150-common/sepolicy"
+PATH_KERNEL="kernel/motorola/sm7150"
 
 # Setup
-${CLONE} ${STAGING} ${GITHUB}/${SM6150_COMMON}/${REPO_DEVICE} ${PATH_DEVICE}
-${CLONE} ${STAGING} ${GITHUB}/${SM6150_COMMON}/${REPO_DEVICE_COMMON} ${PATH_DEVIE_COMMON}
-${CLONE} ${STAGING} ${GITHUB}/${SM6150_COMMON}/${REPO_VENDOR_COMMON} ${PATH_VENDOR_COMMON}
-${CLONE} ${STAGING} ${GITHUB}/${SM6150_COMMON}/${REPO_KERNEL} ${PATH_KERNEL}
-${CLONE} ${STAGING} ${GITHUB}/${SM6150_COMMON}/${REPO_SEPOLICY} ${PATH_SEPOLICY}
+${CLONE} ${STAGING} ${GITHUB}/${SM7150_COMMON}/${REPO_DEVICE} ${PATH_DEVICE}
+${CLONE} ${STAGING} ${GITHUB}/${SM7150_COMMON}/${REPO_DEVICE_COMMON} ${PATH_DEVIE_COMMON}
+${CLONE} ${STAGING} ${GITHUB}/${SM7150_COMMON}/${REPO_VENDOR_COMMON} ${PATH_VENDOR_COMMON}
+${CLONE} ${STAGING} ${GITHUB}/${SM7150_COMMON}/${REPO_KERNEL} ${PATH_KERNEL}
+${CLONE} ${STAGING} ${GITHUB}/${SM7150_COMMON}/${REPO_SEPOLICY} ${PATH_SEPOLICY}
